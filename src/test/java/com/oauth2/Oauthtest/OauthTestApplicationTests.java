aba181e3969fdbac5fca1db925eeec4b34f9ce2c
Santhosh <santhosh@Santhoshs-MacBook-Pro.local>
2024-02-01 20:03:17 -0600

    initial commit

---
+++ src/test/java/com/oauth2/Oauthtest/OauthTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.Oauthtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
